6018bbafdc5c16753c7b50f42ed6b968f679d355
Aaron Williams <aaronrw4@yahoo.com>
2021-07-23 14:33:26 -0700

    Set up App.js

--- sportsbook/src/App.js
+++ sportsbook/src/App.js
@@ -1,8 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import {Route} from 'react-router-dom';
+import axios from 'axios';
+import { StatsContext } from './Context/StatsContext';
+import { ResultsContext } from './Context/ResultsContext';
 import './App.css';
+import Results from './Components/results'
 
 function App() {
+  const [stats, setStats] = useState({});
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {   
+    function fetchData(address, name) {
+      axios
+          .get(address)
+          .then(res => {
+            setStats(prevState => ({...prevState, [name]: res.data}))
+          })
+          .catch(err => console.log(err))    
+    }
+    
+    fetchData('http://127.0.0.1:5000/api/nba-season/all', 'Season')
+    fetchData('http://127.0.0.1:5000/api/nba-last10/all', 'Last 10')
+  },[])
+  
   return (
     <div className="App">
+      <StatsContext.Provider value={stats}>
+        <ResultsContext.Provider value={results}>
+          <Route exact path="/" component={Results}/>
+        </ResultsContext.Provider>
+      </StatsContext.Provider>
     </div>
   );
 }
