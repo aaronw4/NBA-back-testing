865cc27c6464e8f0b5033001cf5380d743c417be
Aaron Williams <aaronrw4@yahoo.com>
2021-07-23 16:12:06 -0700

    Create new data object and add odds data to it

--- sportsbook/src/Functions/resultsData.js
+++ sportsbook/src/Functions/resultsData.js
@@ -6,7 +6,26 @@ export function ResultsData() {
     const odds = stats.Odds
     const season = stats.Season
     const last10 = stats.Last10
-    let data = []
 
-    console.log(odds, season, last10)
+    let oddsKeys
+    if (odds === undefined) {
+        oddsKeys = []
+    } else {
+        oddsKeys = Object.keys(odds)
+    }
+
+    let data = {}
+
+    oddsKeys.map(date => {
+        data[date] = []
+        odds[date].map(game => {
+            let gameStats = {}
+            let gameKeys = Object.keys(game)
+            gameKeys.map(stat => {
+                gameStats[stat] = game[stat]    
+            })
+        })
+    })
+
+    console.log(odds, oddsKeys)
 }
