1d8e6f4c7fec49e8d1c566d1ecc9d3bf0c6f20ae
Aaron Williams <aaronrw4@yahoo.com>
2021-07-30 14:30:16 -0700

    Added difference argument

--- sportsbook/src/Functions/pick.js
+++ sportsbook/src/Functions/pick.js
@@ -1,14 +1,14 @@
-export function pickSpread(projecteAwayLine, actualAwayLine) {
+export function pickSpread(projecteAwayLine, actualAwayLine, difference) {
     let pick
     let condition
 
     if (projecteAwayLine === '') {
         pick = 'No bet'
         condition = ''
-    } else if (Number(actualAwayLine) - Number(projecteAwayLine) >= 4) {
+    } else if (Number(actualAwayLine) - Number(projecteAwayLine) >= Number(difference)) {
         pick = 'Away'
         condition = Number(actualAwayLine)
-    } else if (Number(actualAwayLine) - Number(projecteAwayLine) <= -4) {
+    } else if (Number(actualAwayLine) - Number(projecteAwayLine) <= -Number(difference)) {
         pick = 'Home'
         condition = -1 * Number(actualAwayLine)
     } else {
@@ -19,14 +19,14 @@ export function pickSpread(projecteAwayLine, actualAwayLine) {
     return [pick, condition]
 }
 
-export function pickTotal(projecteTotal, lineTotal) {
+export function pickTotal(projecteTotal, lineTotal, difference) {
     let pick
 
     if (projecteTotal === '') {
         pick = 'No bet'
-    } else if (Number(lineTotal) - Number(projecteTotal) >= 4) {
+    } else if (Number(lineTotal) - Number(projecteTotal) >= Number(difference)) {
         pick = 'Under'
-    } else if (Number(lineTotal) - Number(projecteTotal) <= -4) {
+    } else if (Number(lineTotal) - Number(projecteTotal) <= -Number(difference)) {
         pick = 'Over'
     } else {
         pick = 'No bet'
