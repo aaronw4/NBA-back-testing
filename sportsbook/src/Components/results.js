a41715646e04d4714f0f3f68fe408f73b9f305af
Aaron Williams <aaronrw4@yahoo.com>
2021-08-02 08:17:18 -0700

    Added different results to count array

--- sportsbook/src/Components/results.js
+++ sportsbook/src/Components/results.js
@@ -7,18 +7,18 @@ const Results = () => {
     const {inputResults} = useContext(ResultsContext)
     let count = []
 
-    count.push(resultCount('Season +4', 'sp4ResultSpread', 'sp4ResultTotal'))
-    count.push(resultCount('Season +3', 'sp3ResultSpread', 'sp3ResultTotal'))
-    count.push(resultCount('Season +2', 'sp2ResultSpread', 'sp2ResultTotal'))
-    count.push(resultCount('Season Away/Home +4', 'slp4ResultSpread', 'slp4ResultTotal'))
-    count.push(resultCount('Season Away/Home +3', 'slp3ResultSpread', 'slp3ResultTotal'))
-    count.push(resultCount('Season Away/Home +2', 'slp2ResultSpread', 'slp2ResultTotal'))
-    count.push(resultCount('Last Ten +4', 'lp4ResultSpread', 'lp4ResultTotal'))
-    count.push(resultCount('Last Ten +3', 'lp3ResultSpread', 'lp3ResultTotal'))
-    count.push(resultCount('Last Ten +2', 'lp2ResultSpread', 'lp2ResultTotal'))
-    count.push(resultCount('Last Ten Away/Home +4', 'llp4ResultSpread', 'llp4ResultTotal'))
-    count.push(resultCount('Last Ten Away/Home +3', 'llp3ResultSpread', 'llp3ResultTotal'))
-    count.push(resultCount('Last Ten Away/Home +2', 'llp2ResultSpread', 'llp2ResultTotal'))
+    count.push(resultCount('Season +4', 'sp4ResultSpread', 'sp4ResultTotal', 'sp', 'sp4'))
+    count.push(resultCount('Season +3', 'sp3ResultSpread', 'sp3ResultTotal', 'sp', 'sp3'))
+    count.push(resultCount('Season +2', 'sp2ResultSpread', 'sp2ResultTotal', 'sp', 'sp2'))
+    count.push(resultCount('Season Away/Home +4', 'slp4ResultSpread', 'slp4ResultTotal', 'slp', 'slp4'))
+    count.push(resultCount('Season Away/Home +3', 'slp3ResultSpread', 'slp3ResultTotal', 'slp', 'slp3'))
+    count.push(resultCount('Season Away/Home +2', 'slp2ResultSpread', 'slp2ResultTotal', 'slp', 'slp2'))
+    count.push(resultCount('Last Ten +4', 'lp4ResultSpread', 'lp4ResultTotal', 'lp', 'lp4'))
+    count.push(resultCount('Last Ten +3', 'lp3ResultSpread', 'lp3ResultTotal', 'lp', 'lp3'))
+    count.push(resultCount('Last Ten +2', 'lp2ResultSpread', 'lp2ResultTotal', 'lp', 'lp2'))
+    count.push(resultCount('Last Ten Away/Home +4', 'llp4ResultSpread', 'llp4ResultTotal', 'llp', 'llp4'))
+    count.push(resultCount('Last Ten Away/Home +3', 'llp3ResultSpread', 'llp3ResultTotal', 'llp', 'llp3'))
+    count.push(resultCount('Last Ten Away/Home +2', 'llp2ResultSpread', 'llp2ResultTotal', 'llp', 'llp3'))
 
     return (
         <div>
