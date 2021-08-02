24145a8221f88a21782f44ce2099b62cd6bd3f6f
Aaron Williams <aaronrw4@yahoo.com>
2021-08-02 10:20:39 -0700

    Created resultsDetailsDate.js

---
+++ sportsbook/src/Components/resultsDetailsDate.js
@@ -0,0 +1,52 @@
+import React, {useContext} from 'react';
+import { ResultsContext } from '../Context/ResultsContext';
+
+const ResultsDetailsDate = (props) => {
+    const {results} = useContext(ResultsContext)
+    let dateArray = props.dateArray
+    return (
+        <div>
+        {dateArray.map(game => (
+            <div className='detailsDateCont'>
+                <div className='details'>
+                    <h5>Teams</h5>
+                    <p>{game.team_away}</p>
+                    <p>{game.team_home}</p>
+                </div>
+                <div className='details'>
+                    <h5>{results.name} Projected</h5>
+                    <p>{game[results.dataType+'ScoreAway']}({game[results.dataType+'SpreadAway']})</p>
+                    <p>{game[results.dataType+'ScoreHome']}({game[results.dataType+'SpreadHome']})</p>
+                    <p>{game[results.dataType+'Total']}</p>
+                </div>
+                <div className='details'>
+                    <h5>Lines</h5>
+                    <p>{game.spread_away}</p>
+                    <p>{game.spread_home}</p>
+                    <p>{game.total}</p>
+                </div>
+                <div className='details'>
+                    <h5>Pick</h5>
+                    <p>{game[results.pickDiff+'SpreadPick']}</p>
+                    <br></br>
+                    <p>{game[results.pickDiff+'TotalPick']}</p>
+                </div>
+                <div className='details'>
+                    <h5>Actual</h5>
+                    <p>{game.score_away}({game.score_home-game.score_away})</p>
+                    <p>{game.score_home}({game.score_away-game.score_home})</p>
+                    <p>{game.score_home+game.score_away}</p>
+                </div>
+                <div className='details'>
+                    <h5>Results</h5>
+                    <p>{game[results.pickDiff+'ResultSpread']}</p>
+                    <br></br>
+                    <p>{game[results.pickDiff+'ResultTotal']}</p>
+                </div>
+            </div>
+        ))}
+        </div>
+    )
+}
+
+export default ResultsDetailsDate
