df241e1b39da03a7906775ece23b74dedf45aaac
Aaron Williams <aaronrw4@yahoo.com>
2021-08-25 19:35:16 -0700

    Added away/home league avg to parameters

--- sportsbook/src/Projections/last10LocProj.js
+++ sportsbook/src/Projections/last10LocProj.js
@@ -7,8 +7,10 @@ export function Last10LocProj(date, awayTeam, homeTeam) {
     const last10Away = stats.Last10Away[date]
     const last10Home = stats.Last10Home[date]
     const leagueAve = stats.SeasonLeagueAvg[date]
+    const leagAveAway = stats.SeasonLeagueAveAway[date]
+    const leagAveHome = stats.SeasonLeagueAveHome[date]
     const away = last10Away[awayTeam]
     const home = last10Home[homeTeam]
     
-    return projected(away, home, leagueAve)
+    return projected(away, home, leagueAve, leagAveAway, leagAveHome)
 }
--- sportsbook/src/Projections/seasonLocProj.js
+++ sportsbook/src/Projections/seasonLocProj.js
@@ -7,8 +7,10 @@ export function SeasonLocProj(date, awayTeam, homeTeam) {
     const seasonAway = stats.SeasonAway[date]
     const seasonHome = stats.SeasonHome[date]
     const leagueAve = stats.SeasonLeagueAvg[date]
+    const leagAveAway = stats.SeasonLeagueAveAway[date]
+    const leagAveHome = stats.SeasonLeagueAveHome[date]
     const away = seasonAway[awayTeam]
     const home = seasonHome[homeTeam]
     
-    return projected(away, home, leagueAve)
+    return projected(away, home, leagueAve, leagAveAway, leagAveHome)
 }
