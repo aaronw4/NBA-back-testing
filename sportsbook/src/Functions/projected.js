ec9a177006e0c5a2b638fbe3cabe4cb02c454106
Aaron Williams <aaronrw4@yahoo.com>
2021-08-25 19:33:00 -0700

    Adjusted offense w/ FT. and away/home leag avg

--- sportsbook/src/Functions/projected.js
+++ sportsbook/src/Functions/projected.js
@@ -1,4 +1,4 @@
-export function projected(away, home, leagueAve) {
+export function projected(away, home, leagueAve, awayLeagAve = leagueAve, homeLeagAve = leagueAve) {
     if (away === undefined || home === undefined) {
         return ([{
             scoreAway: null,
@@ -11,15 +11,21 @@ export function projected(away, home, leagueAve) {
         }])
     }
 
-    let awayPoss = (away.fga + away.tov + 0.475*(away.fta) - away.orb)
-    let homePoss = (home.fga + home.tov + 0.475*(home.fta) - home.orb)
-    let avgPoss = (leagueAve.fga + leagueAve.tov + 0.475*(leagueAve.fta) - leagueAve.orb)
+    let awayPoss = (away.fga + away.tov + 0.5*(away.fta) - away.orb)
+    let homePoss = (home.fga + home.tov + 0.5*(home.fta) - home.orb)
+    let avgPoss = (leagueAve.fga + leagueAve.tov + 0.5*(leagueAve.fta) - leagueAve.orb)
     let possessions = (awayPoss/avgPoss)*homePoss
 
-    let awayAdjO = away.points / awayPoss
-    let awayAdjD = away.points_opp / awayPoss
-    let homeAdjO = home.points / homePoss
-    let homeAdjD = home.points_opp / homePoss
+    let awayFT = away.points - 3*away.three - 2*(away.fg - away.three)
+    let awayFT_opp = away.points_opp - 3*away.three_opp - 2*(away.fg_opp - away.three_opp)
+    let homeFT = home.points - 3*home.three - 2*(home.fg - home.three)
+    let homeFT_opp = home.points_opp - 3*home.three_opp - 2*(home.fg_opp - home.three_opp)
+    let leagueAveFT = leagueAve.points - 3*leagueAve.three - 2*(leagueAve.fg - leagueAve.three)
+
+    let awayAdjO = (away.points - awayFT) / awayPoss
+    let awayAdjD = (away.points_opp - awayFT_opp) / awayPoss
+    let homeAdjO = (home.points - homeFT) / homePoss
+    let homeAdjD = (home.points_opp - homeFT_opp) / homePoss
 
     let pythAway = awayAdjO**14 / (awayAdjO**14 + awayAdjD**14)
     let pythHome = homeAdjO**14 / (homeAdjO**14 + homeAdjD**14)
@@ -41,9 +47,9 @@ export function projected(away, home, leagueAve) {
         moneylineHome = -100 / (decHome - 1)
     }
     
-    let scoreAway = (awayAdjO*possessions/leagueAve.points)*homeAdjD*possessions
-    let scoreHome = (homeAdjO*possessions/leagueAve.points)*awayAdjD*possessions
-
+    let scoreAway = (awayAdjO*possessions/(awayLeagAve.points - leagueAveFT))*homeAdjD*possessions + awayFT*homeFT_opp/leagueAveFT
+    let scoreHome = (homeAdjO*possessions/(homeLeagAve.points - leagueAveFT))*awayAdjD*possessions + homeFT*awayFT_opp/leagueAveFT
+    
     let spreadHome = (scoreAway - scoreHome).toFixed(1)
     let spreadAway = (scoreHome - scoreAway).toFixed(1)
     let total = (scoreAway + scoreHome).toFixed(1)
