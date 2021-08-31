3599707f7385da7032c14a4163f746fc84c7ad92
Aaron Williams <aaronrw4@yahoo.com>
2021-08-30 21:59:18 -0700

    Added point totals

--- sportsbook/src/Functions/actualResults.js
+++ sportsbook/src/Functions/actualResults.js
@@ -3,6 +3,9 @@ import { ResultsData } from "./resultsData"
 export function actualResults() {
     const resultsData = ResultsData()
     let resultKeys = Object.keys(resultsData)
+    let pointsAway = []
+    let pointsHome = []
+    let vegasPoints = []
     let overWins = 0
     let underWins = 0
     let dogWins = 0
@@ -25,14 +28,35 @@ export function actualResults() {
             } else if (game.score_home + game.score_away > game.total) {
                 overWins++
             }
+
+            if (game.score_away === 'Cancelled') {
+                let nothing = 0
+            } else {
+                vegasPoints.push(game.total)
+                pointsAway.push(game.score_away)
+                pointsHome.push(game.score_home)
+            }   
         })
     })
+
+    function reducer(total, num) {
+        return total + num
+    }
+
+    let pointsAwayTotal = pointsAway.reduce(reducer)
+    let pointsHomeTotal = pointsHome.reduce(reducer)
+    let pointsTotal = pointsAwayTotal + pointsHomeTotal
+    let vegasPointsSum = vegasPoints.reduce(reducer)
+    let pointsAvg = pointsTotal / pointsAway.length
+    let vegasPointsAve = vegasPointsSum / vegasPoints.length
 
     let data = {
         'overWins': overWins,
         'underWins': underWins,
         'dogWins': dogWins,
-        'favWins': favWins
+        'favWins': favWins,
+        'pointsAvg': pointsAvg,
+        'vegasPointsAve': vegasPointsAve
     }
 
     return data
