8ef4a63d2c3fdf5fc81b58a059991079a3d9f020
Aaron Williams <aaronrw4@yahoo.com>
2021-08-31 13:02:42 -0700

    Created compareDetailsDate

---
+++ sportsbook/src/Components/compareDetailsDate.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const CompareDetailsDate = (props) => {
+    let dateArray = props.dateArray
+
+    return (
+        <div>
+            {dateArray.map(game => (
+                <div className='detailsDateCont'>
+                    <div className='details'>
+                        <h5>Teams</h5>
+                        <p>{game.team_away}</p>
+                        <p>{game.team_home}</p>
+                    </div>
+                    <div className='details'>
+                        <h5>Vegas</h5>
+                        <p>{(game.total - game.spread_away) / 2}</p>
+                        <p>{(game.total - game.spread_home) / 2}</p>
+                    </div>
+                    <div className='details'>
+                        <h5>Season Projected</h5>
+                        <p>{game.spScoreAway}</p>
+                        <p>{game.spScoreHome}</p>
+                    </div>
+                    <div className='details'>
+                        <h5>Season (Away/Home) Projected</h5>
+                        <p>{game.slpScoreAway}</p>
+                        <p>{game.slpScoreHome}</p>
+                    </div>
+                    <div className='details'>
+                        <h5>Last Ten Projected</h5>
+                        <p>{game.lpScoreAway}</p>
+                        <p>{game.lpScoreHome}</p>
+                    </div>
+                    <div className='details'>
+                        <h5>Last Ten (Away/Home) Projected</h5>
+                        <p>{game.llpScoreAway}</p>
+                        <p>{game.llpScoreHome}</p>
+                    </div>
+                    <div className='details'>
+                        <h5>Actual</h5>
+                        <p>{game.score_away}</p>
+                        <p>{game.score_home}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default CompareDetailsDate
