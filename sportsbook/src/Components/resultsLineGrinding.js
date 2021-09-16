0cd9bc8f31f2db3bb77df6c4fc8bbd1cbbf37b60
Aaron Williams <aaronrw4@yahoo.com>
2021-09-15 17:49:39 -0700

    Changed name and fixed class name

--- sportsbook/src/Components/resultsLineGrinding.js
+++ sportsbook/src/Components/resultsLineGrinding.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 import { ResultsContext } from "../Context/ResultsContext";
 import { ResultCount } from "./resultCount";
 
-const LineGrindingResults = () => {
+const ResultsLineGrinding = () => {
     const {inputResults} = useContext(ResultsContext)
     let count = []
 
@@ -14,7 +14,7 @@ const LineGrindingResults = () => {
             <h2 className='header'>Line Grinding Results</h2>
             <div className='resultsCont'>
                 {count.map(results => (
-                    <Link className='resultsItm' onClick={() => inputResults(results)} to='/lineGrinding-details'>
+                    <Link className='resultsItem' onClick={() => inputResults(results)} to='/lineGrinding-details'>
                         <h5>{results.name}</h5>
                         <p>Spread Results: {results.spreadWon}/{results.spreadTotal} ({(results.spreadWon/results.spreadTotal*100).toFixed(1)}%)</p>
                         <p>Total Results: {results.totalWon}/{results.totalTotal}  ({(results.totalWon/results.totalTotal*100).toFixed(1)}%)</p>
@@ -25,4 +25,4 @@ const LineGrindingResults = () => {
     )
 }
 
-export default LineGrindingResults
+export default ResultsLineGrinding
