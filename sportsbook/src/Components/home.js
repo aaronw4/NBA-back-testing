8e748bb17bdd2bb46c35bfe18b0cb1729d4909c7
Aaron Williams <aaronrw4@yahoo.com>
2021-09-18 14:15:12 -0700

    Added content to home page

--- sportsbook/src/Components/home.js
+++ sportsbook/src/Components/home.js
@@ -3,17 +3,47 @@ import {Link} from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            <br/>
-            <Link to='/results'>
-                <button>See Projected Results</button>
-            </Link>
-            <Link to='/results-lineGrinding'>
-                <button>See Line Grinding Results</button>
-            </Link>
-            <Link to='/results-vegas'>
-                <button>See Vegas Results</button>
-            </Link>
+        <div className='homeCont'>
+            <div class="card text-white bg-secondary mb-3">
+                <h3 class="card-header">Handicapping Method</h3>
+                <div class="card-body">
+                    <p class="card-text">
+                        In this method, NBA team stats were used to calculate expected game scores. 
+                        Subtracting the two scores provided the projected spread, while adding the 
+                        two scores provided the projected total. In the simulation, a bet was made 
+                        if the projected result was off the vegas line by four points or more. Several 
+                        different sets of data were used and compared to each other.
+                    </p>
+                    <Link className='link' to='/results'>
+                        <button class="btn btn-primary">See Sumary</button>
+                    </Link>
+                </div>
+            </div>
+            <div class="card text-white bg-secondary mb-3">
+                <h3 class="card-header">Line Movement Method</h3>
+                <div class="card-body">
+                    <p class="card-text">
+                        In this method, the Vegas lines for spread and totals were compared to their opening lines. 
+                        If a line moved by 1 point or more, then a bet would be made on the side with the improved odds. 
+                    </p>
+                    <Link className='link' to='/results-lineGrinding'>
+                        <button class="btn btn-primary">See Sumary</button>
+                    </Link>
+                </div>
+            </div>
+            <div class="card text-white bg-secondary mb-3">
+                <h3 class="card-header">Vegas Predictions</h3>
+                <div class="card-body">
+                    <p class="card-text">
+                        Here the scores of games were predicted from Vegas's lines. A formula was used to find the scores 
+                        that would match the spreads and totals for each game. These Vegas predictions were then compared 
+                        to actual game results.
+                    </p>
+                    <Link className='link' to='/results-vegas'>
+                        <button class="btn btn-primary">See Sumary</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
