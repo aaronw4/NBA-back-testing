dc380ae2d5f1a841ccc71c18bd24faed67a340da
Aaron Williams <aaronrw4@yahoo.com>
2021-09-17 18:01:46 -0700

    Created title banner

---
+++ sportsbook/src/Components/titleBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TitleBanner = () => {
+    return (
+        <div className='banner'>
+            <h1>NBA Sports Betting Analysis: 2020/2021 Season</h1>
+        </div>
+    )
+}
+
+export default TitleBanner
