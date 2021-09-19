29af52f98995234904166ff128d9ea005b4841d0
Aaron Williams <aaronrw4@yahoo.com>
2021-09-19 15:42:41 -0700

    Created home button

--- sportsbook/src/Components/titleBanner.js
+++ sportsbook/src/Components/titleBanner.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const TitleBanner = () => {
     return (
         <div className='banner'>
+            <Link to='/'>
+                <button type="button" class="btn btn-outline-dark home">Home</button>
+            </Link>
             <h1>NBA Sports Betting Analysis: 2020/2021 Season</h1>
         </div>
     )
