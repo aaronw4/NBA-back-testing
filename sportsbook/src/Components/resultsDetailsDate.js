9495486fe634559aa20a45fe8121cb615d1ee04f
Aaron Williams <aaronrw4@yahoo.com>
2021-09-19 15:20:59 -0700

    Converted html to table format

--- sportsbook/src/Components/resultsDetailsDate.js
+++ sportsbook/src/Components/resultsDetailsDate.js
@@ -6,53 +6,50 @@ const ResultsDetailsDate = (props) => {
     let dateArray = props.dateArray
 
     return (
-        <div>
-        {dateArray.map(game => (
-            <div className='detailsDateCont'>
-                <div className='details'>
-                    <h5>Teams</h5>
-                    <p>{game.team_away}</p>
-                    <p>{game.team_home}</p>
-                </div>
-                <div className='details'>
-                    <h5>{selected.name} Projected</h5>
-                    <p>{game[selected.dataType+'ScoreAway']}({game[selected.dataType+'SpreadAway']})</p>
-                    <p>{game[selected.dataType+'ScoreHome']}({game[selected.dataType+'SpreadHome']})</p>
-                    <p>{game[selected.dataType+'Total']}</p>
-                </div>
-                <div className='details'>
-                    <h5>Vegas</h5>
-                    <p>{(game.total - game.spread_away) / 2}</p>
-                    <p>{(game.total - game.spread_home) / 2}</p>
-                    <br></br>
-                </div>
-                <div className='details'>
-                    <h5>Lines</h5>
-                    <p>{game.spread_away}</p>
-                    <p>{game.spread_home}</p>
-                    <p>{game.total}</p>
-                </div>
-                <div className='details'>
-                    <h5>Pick</h5>
-                    <p>{game[selected.dataType+selected.pickDiff+'SpreadPick']}</p>
-                    <br></br>
-                    <p>{game[selected.dataType+selected.pickDiff+'TotalPick']}</p>
-                </div>
-                <div className='details'>
-                    <h5>Actual</h5>
-                    <p>{game.score_away}({game.score_home-game.score_away})</p>
-                    <p>{game.score_home}({game.score_away-game.score_home})</p>
-                    <p>{game.score_home+game.score_away}</p>
-                </div>
-                <div className='details'>
-                    <h5>Results</h5>
-                    <p className={game[selected.dataType+selected.pickDiff+'ResultSpread']}>{game[selected.dataType+selected.pickDiff+'ResultSpread']}</p>
-                    <br></br>
-                    <p className={game[selected.dataType+selected.pickDiff+'ResultTotal']}>{game[selected.dataType+selected.pickDiff+'ResultTotal']}</p>
-                </div>
-            </div>
-        ))}
-        </div>
+        dateArray.map(game => (
+            <table class="table table-bordered table-secondary">
+                <thead>
+                    <tr>
+                        <th scope="col">Teams</th>
+                        <th scope="col">{selected.name} Projected</th>
+                        <th scope="col">Vegas Points</th>
+                        <th scope="col">Vegas Lines</th>
+                        <th scope='col'>Pick</th>
+                        <th scope='col'>Actual</th>
+                        <th scope='col'>Results</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{game.team_away}</td>
+                        <td>{game[selected.dataType+'ScoreAway']}({game[selected.dataType+'SpreadAway']})</td>
+                        <td>{(game.total - game.spread_away) / 2}</td>
+                        <td>{game.spread_away}</td>
+                        <td>{game[selected.dataType+selected.pickDiff+'SpreadPick']}</td>
+                        <td>{game.score_away}({game.score_home-game.score_away})</td>
+                        <td class={game[selected.dataType+selected.pickDiff+'ResultSpread']}>{game[selected.dataType+selected.pickDiff+'ResultSpread']}</td>
+                    </tr>
+                    <tr>
+                        <td>{game.team_home}</td>
+                        <td>{game[selected.dataType+'ScoreHome']}({game[selected.dataType+'SpreadHome']})</td>
+                        <td>{(game.total - game.spread_home) / 2}</td>
+                        <td>{game.spread_home}</td>
+                        <td></td>
+                        <td>{game.score_home}({game.score_away-game.score_home})</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>{game[selected.dataType+'Total']}</td>
+                        <td></td>
+                        <td>{game.total}</td>
+                        <td>{game[selected.dataType+selected.pickDiff+'TotalPick']}</td>
+                        <td>{game.score_home+game.score_away}</td>
+                        <td class={game[selected.dataType+selected.pickDiff+'ResultTotal']}>{game[selected.dataType+selected.pickDiff+'ResultTotal']}</td>
+                    </tr>
+                </tbody>
+            </table>
+        ))
     )
 }
 
