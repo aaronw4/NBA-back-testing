81e9b20b3065a6eb1966390599483748a45228b0
Aaron Williams <aaronrw4@yahoo.com>
2021-09-19 20:17:49 -0700

    Reformatted html to new style

--- sportsbook/src/Components/resultsVegas.js
+++ sportsbook/src/Components/resultsVegas.js
@@ -7,18 +7,27 @@ const ResultsVegas = () => {
 
     return (
         <div>
-            <h2 className='header'>Vegas Line Results</h2>
-            <div>
-                <p>Average Points per Game: {results.pointsAvg}</p>
-                <p>Vegas Avg Points per Game: {results.vegasPointsAve}</p>
-                <p>Games Over: {results.overWins}</p>
-                <p>Games Under: {results.underWins}</p>
-                <p>Favorite Covered: {results.favWins}</p>
-                <p>Dog Covered: {results.dogWins}</p>
+            <div className='header'>
+                <h2>Vegas Line Summary</h2>
             </div>
-            <Link to='vegas-details'>
-                <button>Details</button>
-            </Link>
+            <div className='resultsCont'>
+                <div class="card text-white bg-secondary mb-3 hcSummary">
+                    <h5 class="card-header">Vegas vs Actual Results</h5>
+                    <div>
+                        <p class='hcP'>Average Points per Game: {results.pointsAvg}</p>
+                        <p>Vegas Avg Points per Game: {results.vegasPointsAve}</p>
+                        <p>Games Over: {results.overWins}</p>
+                        <p>Games Under: {results.underWins}</p>
+                        <p>Favorite Covered: {results.favWins}</p>
+                        <p>Dog Covered: {results.dogWins}</p>
+                    </div>
+                    <Link to='vegas-details'>
+                        <button class="btn btn-primary">
+                            Details
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
