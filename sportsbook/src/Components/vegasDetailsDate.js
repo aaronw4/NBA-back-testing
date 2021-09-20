052480e141addaaaf491cd7dd8a621600dd76f82
Aaron Williams <aaronrw4@yahoo.com>
2021-09-19 20:18:34 -0700

    Reformatted html to table format

--- sportsbook/src/Components/vegasDetailsDate.js
+++ sportsbook/src/Components/vegasDetailsDate.js
@@ -1,40 +1,71 @@
 import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
 import { ResultsContext } from '../Context/ResultsContext';
 
 const VegasDetailsDate = (props) => {
-    const {results} = useContext(ResultsContext)
+    const {selected} = useContext(ResultsContext)
     let dateArray = props.dateArray
 
     return (
-        <div>
-            {dateArray.map(game => (
-                <div className='detailsDateCont'>
-                    <div className='details'>
-                        <h5>Teams</h5>
-                        <p>{game.team_away}</p>
-                        <p>{game.team_home}</p>
-                    </div>
-                    <div className='details'>
-                        <h5>Lines</h5>
-                        <p>{game.spread_away}</p>
-                        <p>{game.spread_home}</p>
-                        <p>{game.total}</p>
-                    </div>
-                    <div className='details'>
-                        <h5>Vegas</h5>
-                        <p>{(game.total - game.spread_away) / 2}</p>
-                        <p>{(game.total - game.spread_home) / 2}</p>
-                        <br></br>
-                    </div>
-                    <div className='details'>
-                        <h5>Actual</h5>
-                        <p>{game.score_away}({game.score_home-game.score_away})</p>
-                        <p>{game.score_home}({game.score_away-game.score_home})</p>
-                        <p>{game.score_home+game.score_away}</p>
-                    </div>
-                </div>
-            ))}
-        </div>
+        dateArray.map(game => (
+            <table class="table table-bordered table-secondary">
+                <Link to='/results-vegas'>
+                    <button type="button" class="btn btn-outline-dark back">Back</button>
+                </Link>
+                <thead>
+                    <tr>
+                        <th scope="col">Teams</th>
+                        <th scope="col">Opening Lines</th>
+                        <th scope="col">Closing Lines</th>
+                        <th scope="col">Opening Vegas Points</th>
+                        <th scope="col">Closing Vegas Points</th>
+                        <th scope='col'>Actual</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{game.team_away}</td>
+                        <td>{game.spread_open_away}</td>
+                        <td>{game.spread_away}</td>
+                        {
+                            !game.total_open || !game.spread_open_away ? 
+                            <td>-</td> :
+                            <td>{(game.total_open - game.spread_open_away) / 2}</td>
+                        }
+                        {
+                            !game.total|| !game.spread_away ? 
+                            <td>-</td> :
+                            <td>{(game.total - game.spread_away) / 2}</td>
+                        }
+                        <td>{game.score_away}({game.score_home-game.score_away})</td>
+                    </tr>
+                    <tr>
+                        <td>{game.team_home}</td>
+                        <td>{game.spread_open_home}</td>
+                        <td>{game.spread_home}</td>
+                        {
+                            !game.total_open || !game.spread_open_home ? 
+                            <td>-</td> :
+                            <td>{(game.total_open - game.spread_open_home) / 2}</td>
+                        }
+                        {
+                            !game.total|| !game.spread_home ? 
+                            <td>-</td> :
+                            <td>{(game.total - game.spread_home) / 2}</td>
+                        }
+                        <td>{game.score_home}({game.score_home-game.score_home})</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>{game.total_open}</td>
+                        <td>{game.total}</td>
+                        <td></td> 
+                        <td></td>
+                        <td>{game.score_home+game.score_away}</td>
+                    </tr>
+                </tbody>
+            </table>
+        ))
     )
 }
 
