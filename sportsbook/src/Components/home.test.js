bf9cca8657addf82cb57d626ddcfc158dcc20af7
Aaron Williams <aaronrw4@yahoo.com>
2021-09-28 21:55:49 -0700

    Tested loading pg leaving + homepage text showing

--- sportsbook/src/Components/home.test.js
+++ sportsbook/src/Components/home.test.js
@@ -2,7 +2,19 @@ import React from 'react';
 import { renderWithRouter, screen } from '../test-utils';
 import Home from './home';
 
-it('Displays loading page', () => {
+it('Displays loading page with no results', () => {
     renderWithRouter(<Home/>)
     screen.getByText('Loading...')
 });
+
+it('Doesnt display loading page with results', () => {
+    const {queryByText} = renderWithRouter(<Home/>, {"stats": 1}, {"results": 2})
+    expect(queryByText(/Loading.../i)).toBeNull()
+})
+
+it('Displays home page', () => {
+    renderWithRouter(<Home/>)
+    screen.getByText('Handicapping Method')
+    screen.getByText('Line Movement Method')
+    screen.getByText('Vegas Predictions')
+})
