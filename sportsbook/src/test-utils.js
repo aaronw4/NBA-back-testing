428b52db6ae655d8ecc22831977c93fff9edace8
Aaron Williams <aaronrw4@yahoo.com>
2021-09-28 21:14:37 -0700

    Created wrapper to include state

--- sportsbook/src/test-utils.js
+++ sportsbook/src/test-utils.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { render } from "@testing-library/react";
+import { StatsContext } from './Context/StatsContext';
+import { ResultsContext } from './Context/ResultsContext';
 
-function renderWithRouter(component) {
-    return (
-        render (
-            <BrowserRouter>
-                {component}
-            </BrowserRouter>
+function renderWithRouter(component, stats = {}, results = {}) {
+    function Wrapper({children}) {
+        return (
+            <StatsContext.Provider value={stats}>
+                <ResultsContext.Provider value={{results}}>
+                    <BrowserRouter>
+                        {children}
+                    </BrowserRouter>
+                </ResultsContext.Provider>
+            </StatsContext.Provider>
         )
+    };
+
+    return (
+        render (component, {wrapper: Wrapper})
     )
 }
 
